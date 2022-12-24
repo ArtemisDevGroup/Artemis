34b1b2871809f9609ecd0c33448930dd0c750552
Douglas Lyman <douglas.lyman@icloud.com>
2022-12-24 04:46:27 +0100

    Changed exception event handlers to log as warnings and not errors.

--- Artemis/ExceptionLoggers.cpp
+++ Artemis/ExceptionLoggers.cpp
@@ -13,17 +13,17 @@ using namespace Artemis;
 
 static Logger* pLog = &Midnight::GetInst()->Log;
 
-void ExceptionHandler(Exception* e) { pLog->LogError(e->GetFunctionName(), e->GetExceptionMessage()); }
-void ParameterExceptionHandler(ParameterException* e) { pLog->LogError(e->GetFunctionName(), "Regarding parameter %s: %s", e->GetParameterName(), e->GetExceptionMessage()); }
-void InstanceInvalidExceptionHandler(InstanceInvalidException* e) { pLog->LogError(e->GetFunctionName(), e->GetExceptionMessage()); }
-void MemoryAccessViolationExceptionHandler(MemoryAccessViolationException* e) { pLog->LogError(e->GetFunctionName(), "%s Operation: %s, Location: 0xllX, Size: %lu", e->GetExceptionMessage(), (e->GetOperationType() == OperationType::Read ? "Read" : "Write"), e->GetAddress(), e->GetSize()); }
-void WindowsApiExceptionHandler(WindowsApiException* e) { pLog->LogError(e->GetFunctionName(), "%s failed with code %lu: %s", e->GetWindowsFunction(), e->GetWindowsCode(), e->GetWindowsMessage()); }
-void ObjectNotFoundExceptionHandler(ObjectNotFoundException* e) { pLog->LogError(e->GetFunctionName(), "%s Regarded Object: %s, Object Type: %s", e->GetExceptionMessage(), e->GetObjectName(), e->GetObjectType()); }
-void NotImplementedExceptionHandler(NotImplementedException* e) { pLog->LogError(e->GetFunctionName(), e->GetExceptionMessage()); }
-void CompatibilityExceptionHandler(CompatibilityException* e) { pLog->LogError(e->GetFunctionName(), e->GetExceptionMessage()); }
-void AttributeExceptionHandler(AttributeException* e) { pLog->LogError(e->GetFunctionName(), "%s Regarded attribute: %s", e->GetExceptionMessage(), e->GetAttributeName()); }
-void IndexOutOfRangeExceptionHandler(IndexOutOfRangeException* e) { pLog->LogError(e->GetFunctionName(), "%s Index: %i, Max allowed index: %i", e->GetExceptionMessage(), e->GetIndex(), e->GetMaxIndex()); }
-void HookExceptionHandler(HookException* e) { pLog->LogError(e->GetFunctionName(), e->GetExceptionMessage()); }
+void ExceptionHandler(Exception* e) { pLog->LogWarning(e->GetFunctionName(), e->GetExceptionMessage()); }
+void ParameterExceptionHandler(ParameterException* e) { pLog->LogWarning(e->GetFunctionName(), "Regarding parameter %s: %s", e->GetParameterName(), e->GetExceptionMessage()); }
+void InstanceInvalidExceptionHandler(InstanceInvalidException* e) { pLog->LogWarning(e->GetFunctionName(), e->GetExceptionMessage()); }
+void MemoryAccessViolationExceptionHandler(MemoryAccessViolationException* e) { pLog->LogWarning(e->GetFunctionName(), "%s Operation: %s, Location: 0xllX, Size: %lu", e->GetExceptionMessage(), (e->GetOperationType() == OperationType::Read ? "Read" : "Write"), e->GetAddress(), e->GetSize()); }
+void WindowsApiExceptionHandler(WindowsApiException* e) { pLog->LogWarning(e->GetFunctionName(), "%s failed with code %lu: %s", e->GetWindowsFunction(), e->GetWindowsCode(), e->GetWindowsMessage()); }
+void ObjectNotFoundExceptionHandler(ObjectNotFoundException* e) { pLog->LogWarning(e->GetFunctionName(), "%s Regarded Object: %s, Object Type: %s", e->GetExceptionMessage(), e->GetObjectName(), e->GetObjectType()); }
+void NotImplementedExceptionHandler(NotImplementedException* e) { pLog->LogWarning(e->GetFunctionName(), e->GetExceptionMessage()); }
+void CompatibilityExceptionHandler(CompatibilityException* e) { pLog->LogWarning(e->GetFunctionName(), e->GetExceptionMessage()); }
+void AttributeExceptionHandler(AttributeException* e) { pLog->LogWarning(e->GetFunctionName(), "%s Regarded attribute: %s", e->GetExceptionMessage(), e->GetAttributeName()); }
+void IndexOutOfRangeExceptionHandler(IndexOutOfRangeException* e) { pLog->LogWarning(e->GetFunctionName(), "%s Index: %i, Max allowed index: %i", e->GetExceptionMessage(), e->GetIndex(), e->GetMaxIndex()); }
+void HookExceptionHandler(HookException* e) { pLog->LogWarning(e->GetFunctionName(), e->GetExceptionMessage()); }
 
 void RegisterEventHandlers() {
 	ExceptionEventManager::GetExceptionEvent() += ExceptionHandler;
