81aac34863fe076ae3c5a79f5a1b6eb9ad3ffc2a
Douglas Lyman <douglas.lyman@icloud.com>
2026-03-18 02:10:00 +0100

    Added doxygen style inline documentation to ExceptionBase.

--- Artemis/API/Exceptions/ExceptionBase.hxx
+++ Artemis/API/Exceptions/ExceptionBase.hxx
@@ -4,6 +4,7 @@
 #include "..\Definitions.hxx"
 
 #include <exception>	// std::exception
+#include <string>		// std::string
 #include <string_view>	// std::string_view
 #include <type_traits>	// std::remove_reference_t
 #include <concepts>		// std::derived_from
@@ -11,22 +12,42 @@
 
 #include <Windows.h>	// DWORD, GetLastError()
 
-#define EXCEPTION_CXX 0xE06D7363	// The underlying exception code raised by Windows for C++ 'throw' statements.
+// The underlying exception code raised by Windows for C++ 'throw' statements.
+#define EXCEPTION_CXX 0xE06D7363
 
 #pragma warning(disable:5272)
 
 namespace Artemis::API {
+	/**
+	 * @brief The base exception type for all Artemis exceptions. Derived from `std::exception`.
+	 */
 	class exception : public std::exception {
 		std::stacktrace _Trace;
 		exception* _InnerException;
 
 	public:
+		/**
+		 * @brief Disables collecting the stacktrace from `std::stacktrace` for the next thrown exception on the current thread.
+		 */
 		ARTEMIS_API static void disable_next_throw_trace() noexcept;
 
+		/**
+		 * @brief Constructs an empty exception.
+		 */
 		ARTEMIS_API exception() noexcept;
 
+		/**
+		 * @brief Constructs an exception with a message.
+		 * @param[in] _Message The exception message.
+		 */
 		ARTEMIS_API exception(std::string_view _Message) noexcept;
 
+		/**
+		 * @brief Constructs an exception with a message and an underlying exception.
+		 * @tparam _Ty The inner exception type. Must be derived from `Artemis::API::exception`.
+		 * @param[in] _Message The exception message.
+		 * @param[in] _InnerException A reference to the underlying exception.
+		 */
 		template<typename _Ty>
 			requires std::derived_from<std::remove_reference_t<_Ty>, exception>
 		inline exception(std::string_view _Message, _Ty&& _InnerException) noexcept : std::exception(_Message.data()), _Trace() {
@@ -34,6 +55,11 @@ namespace Artemis::API {
 			this->_InnerException = new std::remove_reference_t<_Ty>(std::forward<_Ty>(_InnerException));
 		}
 
+		/**
+		 * @brief Constructs an exception with an underlying exception.
+		 * @tparam _Ty The inner exception type. Must be derived from `Artemis::API::exception`.
+		 * @param[in] _InnerException A reference to the underlying exception.
+		 */
 		template<class _Ty>
 			requires std::derived_from<std::remove_reference_t<_Ty>, exception>
 		inline exception(_Ty&& _InnerException) noexcept : exception("An unknown Artemis exception has occured.", std::forward<_Ty>(_InnerException)) {}
@@ -44,11 +70,24 @@ namespace Artemis::API {
 
 		ARTEMIS_API virtual ~exception() noexcept;
 
+		/**
+		 * @brief Gets a generic pointer to the underlying exception of this instance.
+		 * @return A pointer to the inner exception.
+		 */
 		ARTEMIS_API const exception* inner() const noexcept;
 
+		/**
+		 * @brief Gets a specific pointer to the underlying exception of this instance.
+		 * @tparam _Ty The inner exception type. Must be derived from `Artemis::API::exception`.
+		 * @return A pointer to the inner exception.
+		 */
 		template<std::derived_from<exception> _Ty>
 		inline const _Ty* inner() const noexcept { return static_cast<const _Ty*>(this->inner()); }
 
+		/**
+		 * @brief Gets a reference to the stacktrace of this instance.
+		 * @return The instance stacktrace.
+		 */
 		ARTEMIS_API const std::stacktrace& trace() const noexcept;
 
 		exception& operator=(const exception&) = delete;
