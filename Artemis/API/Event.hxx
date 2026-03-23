1304b8f13c1068bb4f6772f193366a94bc089587
Douglas Lyman <douglas.lyman@icloud.com>
2026-03-23 02:19:50 +0100

    Documented Event.hxx

--- Artemis/API/Event.hxx
+++ Artemis/API/Event.hxx
@@ -3,19 +3,25 @@
 
 #include "Definitions.hxx"
 
-#include <cstdint>		// std::uint64_t
-#include <utility>		// std::next
-#include <type_traits>	// std::is_base_of_v
+#include <cstdint>		// std::uint64_t, std::size_t
+#include <concepts>		// std::derived_from
 #include <vector>		// std::vector
 #include <functional>	// std::function
 
 namespace Artemis::API {
+	/**
+	 * @brief The base event args data structure.
+	 */
 	struct event_args {
 		bool _Handled;
 	};
 
-	template<class _Sender = void, class _Ea = event_args>
-		requires(std::is_base_of_v<event_args, _Ea>)
+	/**
+	 * @brief Represents an invocable and subscribable event.
+	 * @tparam _Sender The type of object that is intended to invoke this event. Defaults to `void`.
+	 * @tparam _Ea The event argument data structure type.
+	 */
+	template<class _Sender = void, std::derived_from<event_args> _Ea = event_args>
 	class event {
 		using event_handler = std::function<void(_Sender* _Sender, _Ea* e)>;
 
@@ -27,29 +33,52 @@ namespace Artemis::API {
 	public:
 		event() : _NextId(0) {}
 
+		/**
+		 * @brief Subscribes a handler to the event.
+		 * @param[in] _Handler The handler function.
+		 * @return An id referencing the registered handler.
+		 */
 		std::uint64_t subscribe(event_handler _Handler) {
 			this->_EventHandlers.push_back(_ { _Handler, this->_NextId });
 			return this->_NextId++;
 		}
 
+		/**
+		 * @brief Unsubscribes a handler from the event by id.
+		 * @param[in] _Id The id of the handler to unregister.
+		 */
 		void unsubscribe(std::uint64_t _Id) {
-			for (auto i = this->_EventHandlers.begin(); i != this->_EventHandlers.end(); std::next(i))
+			for (auto i = this->_EventHandlers.begin(); i != this->_EventHandlers.end(); ++i)
 				if (i->_Id == _Id) {
 					this->_EventHandlers.erase(i);
 					return;
 				}
 		}
 
+		/**
+		 * @brief Gets the number of registered handlers.
+		 * @return The number of registered handlers.
+		 */
 		std::size_t handler_count() const noexcept {
 			return _EventHandlers.size();
 		}
 
+		/**
+		 * @brief Unsubscribes all registered handlers.
+		 * @return The number of handlers that were unregistered.
+		 */
 		std::size_t unsubscribe_all() {
 			std::size_t count = this->handler_count();
 			this->_EventHandlers.clear();
 			return count;
 		}
 
+		/**
+		 * @brief Invokes the registered handlers.
+		 * @param[in] _Sender A pointer to the object invoking this event.
+		 * @param[in] e A pointer to the invocation event args.
+		 * @param[in] _DeleteEventArgs Declares whether to delete the object pointed to by `e` after invoking the handlers.
+		 */
 		void invoke(_Sender* _Sender, _Ea* e, bool _DeleteEventArgs) const {
 			for (const _& handler : this->_EventHandlers)
 				handler._Handler(_Sender, e);
